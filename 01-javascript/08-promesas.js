a75c039f362d7a1f8a8c30c57e631a01c4b5c22f
Adrian Eguez <eadepto@hotmail.com>
2023-06-02 17:55:18 -0500

    hasta async await

--- 01-javascript/08-promesas.js
+++ 01-javascript/08-promesas.js
@@ -25,6 +25,95 @@ function leerArchivo(path) {
     ); //
 }
 
-function escribirArchivo(path, contenido){}
+function escribirArchivo(path, nuevoContenido) {
+    return new Promise(
+        (resolve, reject) => {
+            fs.writeFile(
+                path,
+                nuevoContenido,
+                (errorEscritura) => {
+                    if (errorEscritura) {
+                        console.error(errorEscritura);
+                        reject('Error escribiendo nuevo archivo');
+                    } else {
+                        resolve('Completado');
+                    }
+                }
+            );
+        }
+    ); //
+}
 
-function ejercicio(){}
+
+function ejercicio(
+    pathArchivoUno,
+    pathArchivoDos,
+    pathArchivoNuevo
+){
+    let contenidoTotal = '';
+    leerArchivo(pathArchivoUno)
+        .then(
+            (contenidoPrimerArchivo)=>{
+                contenidoTotal = contenidoTotal + contenidoPrimerArchivo;
+                return leerArchivo(pathArchivoDos);
+            }
+        )
+        .then(
+            (contenidoSegundoArchivo)=>{
+                contenidoTotal = contenidoTotal + contenidoSegundoArchivo
+                return escribirArchivo(pathArchivoNuevo, contenidoTotal);
+            }
+        )
+        .catch(error => console.error(error))
+}
+
+const pathUno = '01-variables.js';
+const pathDos = '06-ejemplo.txt';
+const pathTres = '06-ejemplo-respuesta.txt';
+ejercicio(pathUno, pathDos, pathTres);
+
+// ASYNC AWAIT
+// REGLAS:
+// 1) Estar dentro de una funcion (nombrada o anonima)
+// 2) AGREGAR la palabra 'async' antes de la declaracion
+//    de la funcion
+// 3) AGREGAR la palabra 'await' antes de la declaracion
+//    de una promesa
+// const a = async function(){}
+// const a = async ()=>{}
+async function ejercicioConAwait(){
+    const pathUno = '01-variables.js';
+    const pathDos = '06-ejemplo.txt';
+    const pathTres = '06-ejemplo-respuesta.txt';
+    try {
+        const contenidoUno = await leerArchivo(pathUno);
+        const contenidoDos = await leerArchivo(pathDos);
+        const contenidoTotal = contenidoUno + contenidoDos;
+        await escribirArchivo(pathTres, contenidoTotal);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
