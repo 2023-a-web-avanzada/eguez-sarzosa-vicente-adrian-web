a25540d14ba74fa4b7efea0921c4f92ccd1a0c3d
Adrian Eguez <eadepto@hotmail.com>
2023-07-13 18:47:13 -0500

    hasta rutas

--- 03-react/my-app/app/i_react_hook_form/page.tsx
+++ 03-react/my-app/app/i_react_hook_form/page.tsx
@@ -1,14 +1,15 @@
 'use client'
 // i_react_hook_form/page.tsx
 import React, {useState} from "react";
-import {useForm} from "react-hook-form";
+import {Controller, useForm} from "react-hook-form";
 import {FormularioEjemplo} from "@/app/i_react_hook_form/types/formulario-ejemplo";
-import {Button, FormControl, InputLabel} from "@mui/material";
+import {Button, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
 
-export default function Page(){
+export default function Page() {
     const [nombre, setNombre] = useState('Adrian');
 
-    const {handleSubmit, register, formState: {errors, isValid},
+    const {
+        handleSubmit, register, formState: {errors, isValid},
         control
     } = useForm<FormularioEjemplo>(
         {
@@ -24,46 +25,90 @@ export default function Page(){
     }
     return (
         <>
-                <div className="container">
-                    <h1>Formulario con react hook form</h1>
-                    <form onSubmit={handleSubmit(controladorSubmit)}>
-                        <div className="mb-3">
-                            <label htmlFor="nombre" className="form-label">Nombre</label>
-                            <input type="text" className="form-control" placeholder="EJ: Adrian" id="nombre"
-                                   {
-                                        ...register('nombre',{
-                                            required: {
-                                                value: true,
-                                                message: 'Nombre requerido'
-                                            },
-                                            maxLength: {value: 20, message: 'Longitud maxima 20'},
-                                            minLength: {value: 5, message: 'Longitud minima 5'},
-                                        })
-                                   }
-                            />
-                            <div id="nombreHelp" className="form-text">
-                                Ingresa tu nombre.
+            <div className="container">
+                <h1>Formulario con react hook form</h1>
+                <form onSubmit={handleSubmit(controladorSubmit)}>
+                    <div className="mb-3">
+                        <label htmlFor="nombre" className="form-label">Nombre</label>
+                        <input type="text" className="form-control" placeholder="EJ: Adrian" id="nombre"
+                               {
+                                   ...register('nombre', {
+                                       required: {
+                                           value: true,
+                                           message: 'Nombre requerido'
+                                       },
+                                       maxLength: {value: 20, message: 'Longitud maxima 20'},
+                                       minLength: {value: 5, message: 'Longitud minima 5'},
+                                       validate: {
+                                           soloNumeros: (valorActual) => {
+                                               // Transformar a numero un string:
+                                               // Number("1")
+                                               // +"1"
+                                               if (Number.isNaN(+valorActual)) {
+                                                   // Se puede devolver un false o un mensaje de error
+                                                   // return false; // Error
+                                                   return 'Ingrese solo numeros'; // Error
+                                               } else {
+                                                   // Se devuelve un true
+                                                   return true; // Esta correcto
+                                               }
+                                           }
+                                       }
+                                   })
+                               }
+                        />
+                        <div id="nombreHelp" className="form-text">
+                            Ingresa tu nombre.
+                        </div>
+                        {errors.nombre &&
+                            <div className="alert alert-warning"
+                                 role="alert">
+                                Tiene errores {errors.nombre.message}
                             </div>
-                            { errors.nombre &&
-                                <div className="alert alert-warning"
-                                     role="alert">
-                                    Tiene errores {errors.nombre.message}
+                        }
+                    </div>
+                    <div className="mb-3">
+                        <FormControl fullWidth>
+                            <InputLabel id="estadoCivilLabelId">Estado civil</InputLabel>
+                            <Controller
+                                control={control}
+                                rules={{ required: {value: true, message: 'Estado C. requerido'}}}
+                                name="estadoCivil"
+                                render={
+                                    ({field: {onChange, value, onBlur,}}) => {
+                                        return (
+                                            <>
+                                                <Select
+                                                    labelId="estadoCivilLabelId"
+                                                    id="estadoCivilId"
+                                                    label="Estado Civil"
+                                                    onBlur={onBlur}
+                                                    value={value}
+                                                    onChange={onChange as any}
+                                                >
+                                                    <MenuItem value={'casado'}>Casado</MenuItem>
+                                                    <MenuItem value={'soltero'}>Soltero</MenuItem>
+                                                </Select>
+                                            </>
+                                        )
+                                    }
+                                }
+                            />
+                            {/*Termina controller*/}
+                            { errors.estadoCivil &&
+                                <div className="alert alert-warning" role="alert">
+                                    Tiene errores {errors.estadoCivil.message}
                                 </div>
                             }
-                        </div>
-                        <div className="mb-3">
-                            <FormControl fullWidth>
-                                <InputLabel id="estadoCivilLabelId">Estado civil</InputLabel>
-                                { }
-                            </FormControl>
-                        </div>
-                        <Button type="submit"
-                                disabled={!isValid}
-                                variant='outlined'>
-                            Submit
-                        </Button>
-                    </form>
-                </div>
+                        </FormControl>
+                    </div>
+                    <Button type="submit"
+                            disabled={!isValid}
+                            variant='outlined'>
+                        Submit
+                    </Button>
+                </form>
+            </div>
 
         </>
     )
