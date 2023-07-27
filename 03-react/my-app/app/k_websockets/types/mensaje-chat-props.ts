4d7062f1f8703a9e0b40ee73ed474087132da0d0
Adrian Eguez <eadepto@hotmail.com>
2023-07-27 18:40:57 -0500

    hasta mensaje chat

---
+++ 03-react/my-app/app/k_websockets/types/mensaje-chat-props.ts
@@ -0,0 +1,10 @@
+// types/mensaje-chat-props.ts
+export type MensajeChatProps = {
+    nombre: string;
+    mensaje: string;
+    posicion: Posicion
+}
+export enum Posicion{
+    I = 'Izquierda',
+    D = 'Derecha',
+}
